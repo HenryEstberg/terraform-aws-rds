5bfe6b280934a66559817812e09860edc861c11a
Henry Estberg <11286201+HenryEstberg@users.noreply.github.com>
2024-01-24 17:16:05 -0800

    Added default values to variables

--- variables.tf
+++ variables.tf
@@ -1,12 +1,15 @@
 variable "db_name" {
   description = "Unique name to assign to RDS instance"
+  default = "testDB_HenryVariableTest"
 }
 
 variable "db_username" {
   description = "RDS root username"
+  default = "test"
 }
 
 variable "db_password" {
   description = "RDS root user password"
   sensitive   = true
+  default = "test"
 }
